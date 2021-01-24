d021cd0f9bd7e4ad0082a45d6da1ae59c772103d
Junior Fernandes <juniorbass8@gmail.com>
2021-01-24 01:20:19 -0300

    Criando a celular da tableView

---
+++ MVVM/MVVM/View/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  MVVM
+//
+//  Created by Junior Fernandes on 23/01/21.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
